24f235df27a781bbf267417c82ccfdf788f0e637
Maarten Busstra <busstra.m@gmail.com>
2018-01-20 23:44:13 +0100

    update createCard API

--- packages/mobiledoc-cards/src/createCard.js
+++ packages/mobiledoc-cards/src/createCard.js
@@ -1,8 +1,15 @@
+/* eslint-disable react/no-multi-comp */
 import React, { Component } from 'react';
 import { classToDOMCard } from 'react-mobiledoc-editor';
 import PropTypes from 'prop-types';
 
-export default ({ renderWith: ViewComp, editWith: Editor, name }) => {
+export default ({
+  renderWith: ViewComp,
+  editWith: Editor,
+  name,
+  payload: defaultPayload = {},
+  buttonText = '',
+}) => {
   class MobiledocCard extends Component {
     static displayName = name;
     static propTypes = {
@@ -15,10 +22,32 @@ export default ({ renderWith: ViewComp, editWith: Editor, name }) => {
 
     render() {
       const { isInEditor, payload } = this.props;
-      if (!isInEditor) return <ViewComp {...payload} />;
-      return <Editor {...this.props} />;
+      return isInEditor ? <Editor {...this.props} /> : <ViewComp {...payload} />;
     }
   }
 
-  return classToDOMCard(MobiledocCard);
+  class Button extends Component {
+    static displayName = `${name}Button`;
+    static propTypes = {
+      isEditing: PropTypes.bool.isRequired,
+    };
+    static contextTypes = {
+      editor: PropTypes.object,
+    };
+    render() {
+      const { isEditing = false, ...props } = this.props;
+      const { editor } = this.context;
+      return (
+        <button
+          {...props}
+          onClick={() => editor.insertCard(name, defaultPayload, isEditing)}
+        >
+          {buttonText || name}
+        </button>
+      );
+    }
+  }
+  const Card = classToDOMCard(MobiledocCard);
+  Card.Button = Button;
+  return Card;
 };
