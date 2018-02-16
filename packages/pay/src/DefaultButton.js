cdc45a241d9b4bdaa5a8c74e7383728c8d2897dd
Maarten Busstra <busstra.m@gmail.com>
2018-02-16 16:33:45 +0100

    eslint conformity

--- packages/pay/src/DefaultButton.js
+++ packages/pay/src/DefaultButton.js
@@ -4,12 +4,11 @@ import PropTypes from 'prop-types';
 export default class DefaultButton extends Component {
   static propTypes = {
     onPay: PropTypes.func.isRequired,
-    processing: PropTypes.bool.isRequired, // eslint-disable-line react/no-unused-prop-types
-    payment: PropTypes.bool.isRequired, // eslint-disable-line react/no-unused-prop-types
+    processing: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
+    payment: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
   };
 
   static defaultProps = {
-    onPay: () => {},
     processing: false,
     payment: false,
   };
