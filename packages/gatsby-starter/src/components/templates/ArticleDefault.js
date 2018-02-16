b8f81a3ecdf27cb3909e4c27007dd6fed6f5c284
Maarten Busstra <busstra.m@gmail.com>
2018-02-16 16:51:48 +0100

    refactor package references

--- packages/gatsby-starter/src/components/templates/ArticleDefault.js
+++ packages/gatsby-starter/src/components/templates/ArticleDefault.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { compose, setPropTypes, mapProps } from 'recompose';
-import { Content } from '@wingsplatform/gatsby-plugin';
+import { Content } from '@wingsplatform/content';
 
 export default compose(
   mapProps(({ pathContext: { article } }) => ({ article })),
   setPropTypes({
     article: PropTypes.object.isRequired,
   }),
-)(({
-  article,
-}) => (
+)(({ article }) => (
   <div>
     <pre>{JSON.stringify(article, null, 2)}</pre>
     <Content content={article.content} />
