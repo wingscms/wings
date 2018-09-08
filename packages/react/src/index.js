58ec4194a43424f5f9b03809dde33175d37e3355
Maarten Busstra <busstram@gmail.com>
2018-09-08 09:36:43 +0200

    remove slugify util

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -8,4 +8,3 @@ export { default as createCard } from './createCard';
 
 export { default as Platforms } from './components/Platforms';
 export { default as Content } from './components/Content';
-export { slugify } from './lib/utils';
--- packages/react/src/lib/utils.js
+++ packages/react/src/lib/utils.js
@@ -1,12 +1,3 @@
-import _slugify from 'slugify';
-
-// eslint-disable-next-line import/prefer-default-export
-export const slugify = s =>
-  _slugify(s, {
-    remove: /[$*_+~.()'"!\-:@]/g,
-    lower: true,
-  });
-
 export const enumerate = (...args) => {
   const obj = {};
   args.forEach((arg, i) => {
