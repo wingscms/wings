cd3fd625b3233390fe814a1e219bd07298e47030
Mark Jackson <mark.j.jackson@me.com>
2019-05-07 11:56:45 +0200

    update PageDefault to compound component API

--- packages/hummingbird/src/templates/components/PageDefault.js
+++ packages/hummingbird/src/templates/components/PageDefault.js
@@ -6,7 +6,6 @@ import _Content from '../../components/Content';
 import Navigation from '../../components/Navigation';
 import Footer from '../../components/Footer';
 import { StackedHeader } from '../../components/Header';
-import ProgressBar from '../../components/ProgressBar';
 
 import { makeShareUrls, parseBool } from '../../../lib/utils';
 
@@ -19,14 +18,75 @@ const Content = styled(_Content)`
   }
 `;
 
-const Page = styled.article`
+const ContentWrapper = styled.div`
   margin: 0 auto;
   max-width: 760px;
   padding: 0 20px;
-  min-height: 100vh;
+`;
+
+const StyledMenuContentWrapper = styled(MenuContentWrapper)`
+  .slide-menu.chapters {
+    position: fixed;
+    z-index: 50;
+  }
+  &.chaptersOpen {
+    .slide-menu.chapters {
+      position: fixed;
+      margin-left: 300px;
+      top: 0;
+      height: 100vh;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    .slide-menu.chapters {
+      left: -100vw;
+    }
+    &.chaptersOpen {
+      margin-left: 100vw;
+      padding-right: 100vw;
+      width: calc(100% + 100vw);
+      .slide-menu.chapters {
+        margin-left: 100vw;
+      }
+    }
+  }
 `;
 
 export default class PageDefault extends Component {
+  static Navigation = ({
+    entry: {
+      translations,
+      menu,
+      locale,
+      meta: { hideMenu },
+    },
+    headers,
+  }) => (
+    <Navigation
+      chapters={headers}
+      items={menu && menu.items}
+      translations={translations}
+      locale={locale}
+      hideMenu={hideMenu}
+    />
+  );
+
+  static Header = ({ entry }) => (
+    <ContentWrapper>
+      <StackedHeader article={entry} />
+    </ContentWrapper>
+  );
+
+  static Main = ({ entry }) => (
+    <ContentWrapper>
+      <Content className="article-body" id="article-body" content={entry.content} />
+    </ContentWrapper>
+  );
+
+  static defaultProps = {
+    children: [<PageDefault.Navigation />, <PageDefault.Header />, <PageDefault.Main />],
+  };
+
   constructor({ pageContext: { entry }, location }) {
     super();
     this.state = {
@@ -34,34 +94,53 @@ export default class PageDefault extends Component {
     };
   }
 
+  children = () => {
+    const {
+      pageContext: {
+        entry,
+        entry: { translations: _translations },
+        loop,
+        featured,
+      },
+      children,
+    } = this.props;
+    const { headers, shareUrls } = this.state;
+    const translations = Object.keys(_translations || {}).map(_locale => ({
+      _locale,
+      path: _translations[_locale].path,
+    }));
+
+    const childProps = {
+      entry: { ...entry, translations },
+      headers,
+      shareUrls,
+      loop,
+      featured,
+      onHeadersChange: h => this.setState({ headers: h }),
+    };
+
+    return React.Children.map(children, element => React.cloneElement(element, childProps));
+  };
+
   render() {
     const {
-      entry,
-      entry: { meta = {} },
-    } = this.props.pageContext;
-    const { translations } = entry;
-    const { dropCap, hideMenu } = meta;
-    const { shareUrls } = this.state;
+      pageContext: {
+        entry: {
+          meta: { dropCap },
+        },
+      },
+    } = this.props;
     return (
       <Layout>
-        <MenuContentWrapper id="content-wrapper">
-          {entry.type.id === 'article' && <ProgressBar />}
-          <Navigation
-            shareUrls={shareUrls}
-            items={entry.menu && entry.menu.items}
-            translations={Object.keys(translations || {}).map(locale => ({
-              locale,
-              path: entry.translations[locale].path,
-            }))}
-            locale={entry.locale}
-            hideMenu={hideMenu}
-          />
-          <Page className={`page${parseBool(dropCap) ? ' drop-cap' : ''}`}>
-            <StackedHeader article={entry} />
-            <Content className="article-body" id="article-body" content={entry.content} />
-          </Page>
-          <Footer />
-        </MenuContentWrapper>
+        <div id={this.props.id}>
+          <StyledMenuContentWrapper
+            id="content-wrapper"
+            className={`page${parseBool(dropCap) ? ' drop-cap' : ''}`}
+          >
+            {this.children()}
+            <Footer />
+          </StyledMenuContentWrapper>
+        </div>
       </Layout>
     );
   }
