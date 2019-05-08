c37dbdca47a4fff58575d724bd893cf0a899e8ec
Maarten Busstra <busstram@gmail.com>
2019-05-08 12:39:22 +0200

    move button to remove circular dep

--- packages/hummingbird/index.js
+++ packages/hummingbird/index.js
@@ -1,4 +1,4 @@
-export { default as Button } from './src/components/Button';
+export { default as Button } from './src/components/Button/Button';
 
 export { default as Entry } from './src/templates/components/Entry';
 export { default as Article } from './src/templates/components/Article';
