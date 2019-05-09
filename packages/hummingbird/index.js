92ba0e32af16c7ba7be9382dc9473a7483244cfc
Maarten Busstra <busstram@gmail.com>
2019-05-09 15:15:49 +0200

    export Content

--- packages/hummingbird/index.js
+++ packages/hummingbird/index.js
@@ -3,3 +3,5 @@ export { default as Button } from './src/components/Button/Button';
 export { default as Entry } from './src/templates/components/Entry';
 export { default as Article } from './src/templates/components/Article';
 export { default as Page } from './src/templates/components/Page';
+
+export { default as Content } from './src/components/Content';
