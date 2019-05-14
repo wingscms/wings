c4a5febd893e7d454707f596230858832a3bcc8d
Maarten Busstra <busstram@gmail.com>
2019-05-14 02:05:02 +0200

    simplify tpl exports

--- packages/hummingbird/src/templates/Article.js
+++ packages/hummingbird/src/templates/Article.js
@@ -1,10 +1,4 @@
 import React from 'react';
 import { Article } from '@wingscms/hummingbird';
 
-export default props => (
-  <Article {...props}>
-    <Article.CornerMenu />
-    <Article.Header />
-    <Article.Main />
-  </Article>
-);
+export default props => <Article {...props} />;
--- packages/hummingbird/src/templates/Page.js
+++ packages/hummingbird/src/templates/Page.js
@@ -1,9 +1,4 @@
 import React from 'react';
 import { Page } from '@wingscms/hummingbird';
 
-export default props => (
-  <Page {...props}>
-    <Page.StackedHeader />
-    <Page.Main />
-  </Page>
-);
+export default props => <Page {...props} />;
