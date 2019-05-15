38d1f0d0e31b7be790ee864cd60d5683c0aecee4
Maarten Busstra <busstram@gmail.com>
2019-05-15 22:19:59 +0200

    add export campaign

--- packages/hummingbird/index.js
+++ packages/hummingbird/index.js
@@ -1,8 +1,10 @@
+// components
 export { default as Button } from './src/components/Button/Button';
+export { default as Content } from './src/components/Content';
+export { default as LayoutDefault } from './src/components/LayoutDefault';
 
+// templates
 export { default as Entry } from './src/templates/components/Entry';
 export { default as Article } from './src/templates/components/Article';
 export { default as Page } from './src/templates/components/Page';
-
-export { default as Content } from './src/components/Content';
-export { default as LayoutDefault } from './src/components/LayoutDefault';
+export { default as Campaign } from './src/templates/components/Campaign';
