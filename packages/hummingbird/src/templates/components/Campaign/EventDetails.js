bdfb847b36c80287356d00b9902c035498c5122f
Florian Schroiff <florianschroiff@bureaubolster.nl>
2019-05-29 16:44:43 +0200

    fix: add more translations and some localized numbers

--- packages/hummingbird/src/templates/components/Campaign/EventDetails.js
+++ packages/hummingbird/src/templates/components/Campaign/EventDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FormattedMessage, FormattedDate, FormattedTime } from 'react-intl';
 
 const InfoContainer = styled.div``;
 
@@ -33,8 +34,6 @@ const getCurrencySymbol = (currencyCode) => {
   }
 };
 
-const formatMinutes = m => (m < 10 ? `0${m}` : m);
-
 const Wrapper = styled.div`
   display: block;
   position: relative;
@@ -61,27 +60,41 @@ export default ({ title, fee, location, schedule }) => {
       <InfoContainer>
         {schedule.start ? (
           <MetaContainer>
-            <MetaName>Start</MetaName>
+            <MetaName>
+              <FormattedMessage
+                id="app.event.details.startLabel"
+                description="Label for Event Start date"
+                defaultMessage="Start"
+              />
+            </MetaName>
             <MetaContent>
-              {/* eslint-disable-next-line */}
-              {start.getDate()}-{start.getMonth() + 1}-{start.getFullYear()}{' '}
-              {start.getUTCHours() + 1}:{formatMinutes(start.getUTCMinutes())}
+              <FormattedDate value={start} /> <FormattedTime value={start} />
             </MetaContent>
           </MetaContainer>
         ) : null}
         {schedule.end ? (
           <MetaContainer>
-            <MetaName>End</MetaName>
+            <MetaName>
+              <FormattedMessage
+                id="app.event.details.endLabel"
+                description="Label for Event End date"
+                defaultMessage="End"
+              />
+            </MetaName>
             <MetaContent>
-              {/* eslint-disable-next-line */}
-              {end.getDate()}-{end.getMonth() + 1}-{end.getFullYear()} {end.getUTCHours() + 1}:
-              {formatMinutes(end.getUTCMinutes())}
+              <FormattedDate value={end} /> <FormattedTime value={end} />
             </MetaContent>
           </MetaContainer>
         ) : null}
         {location ? (
           <MetaContainer>
-            <MetaName>Location</MetaName>
+            <MetaName>
+              <FormattedMessage
+                id="app.event.details.locationLabel"
+                description="Label for Event Location"
+                defaultMessage="Location"
+              />
+            </MetaName>
             <MetaContent>
               {location.name ? <p>{location.name}</p> : null}
               {location.street ? <p>{location.street}</p> : null}
@@ -93,7 +106,13 @@ export default ({ title, fee, location, schedule }) => {
         ) : null}
         {fee ? (
           <MetaContainer>
-            <MetaName>Price</MetaName>
+            <MetaName>
+              <FormattedMessage
+                id="app.event.details.feeLabel"
+                description="Label for Event Fee"
+                defaultMessage="Price"
+              />
+            </MetaName>
             <MetaContent>
               {getCurrencySymbol(fee.currencyCode)}
               {fee.amount}
