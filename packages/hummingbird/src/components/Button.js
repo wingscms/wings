94915be320a9d4c1c73b4edfaf42efe72e63c096
Mark Jackson <mark.j.jackson@me.com>
2019-06-05 13:44:59 +0200

    fix(hummingbird): move Button to components root

--- packages/hummingbird/src/components/Button.js
+++ packages/hummingbird/src/components/Button.js
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 
 export default styled(Button)`
   font-family: ${({ theme }) => theme.typography.options.headerFontFamily.join(', ')};
+  font-weight: bold;
+  border-radius: 4px;
 `;
