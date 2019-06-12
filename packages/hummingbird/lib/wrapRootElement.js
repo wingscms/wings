7b7901b3a6bd646f0398f1aec2a22eab36c4b543
Florian Schroiff <florianschroiff@bureaubolster.nl>
2019-06-12 15:22:47 +0200

    fix: patch react-intl

--- packages/hummingbird/lib/wrapRootElement.js
+++ packages/hummingbird/lib/wrapRootElement.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/first */
+import './bootstrap';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Provider as PluginOptionsProvider } from '../src/ctx/PluginOptions';
