236c61579f36040456cf282f361fdf2f9d210bf7
Mark Jackson <mark.j.jackson@me.com>
2019-06-20 09:26:24 +0200

    fix: homepage routing fix

--- packages/hummingbird/services/routing.js
+++ packages/hummingbird/services/routing.js
@@ -1,10 +1,15 @@
 module.exports = {
   getPath(node) {
-    const parts = [this.getLocalePrefix(node), this.getPrefix(node), node.slug].filter(p => !!p);
-    return [''].concat(parts).join('/');
+    const parts = [this.getLocalePrefix(node), this.getPrefix(node), this.getSlug(node)].filter(
+      p => !!p,
+    );
+    return [''].concat(parts).join('/') || '/';
   },
   getLocalePrefix(node) {
     return node.locale.primary ? '' : node.locale.id;
+  },
+  getSlug(node) {
+    return node.isHome ? '' : node.slug;
   },
   getPrefix({ resourceType, isHome }) {
     if (isHome) return '';
