f7cd0064430b32f7c9c6a7e8d8c254f12254148c
Mark Jackson <mark.j.jackson@me.com>
2019-07-04 12:07:47 +0200

    feat(hummingbird): update API exports

--- packages/hummingbird/index.js
+++ packages/hummingbird/index.js
@@ -2,12 +2,12 @@
 export { default as Button } from './src/components/Button';
 export { default as Content } from './src/components/Content/Content';
 export { default as LayoutDefault } from './src/components/LayoutDefault';
-export { default as CampaignMain } from './src/components/Campaign';
+export { default as Campaign } from './src/components/Campaign';
 
 // templates
-export { default as Article } from './src/templates/components/Article';
-export { default as Page } from './src/templates/components/Page';
-export { default as Campaign } from './src/templates/components/Campaign';
+export { default as ArticleTemplate } from './src/templates/components/Article';
+export { default as PageTemplate } from './src/templates/components/Page';
+export { default as CampaignTemplate } from './src/templates/components/Campaign';
 
 // services
 export { default as routing } from './services/routing';
