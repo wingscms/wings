adda76f65bf70a0ce33ef4550f19cd8d43aae9c6
Mark Jackson <mark.j.jackson@me.com>
2019-07-04 12:18:02 +0200

    fix: update template component names

--- packages/hummingbird/src/templates/PageHome.js
+++ packages/hummingbird/src/templates/PageHome.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Page } from '@wingscms/hummingbird';
+import { PageTemplate } from '@wingscms/hummingbird';
 
 export default props => (
-  <Page {...props}>
-    <Page.Navigation />
-    <Page.Title />
-    <Page.Main />
-  </Page>
+  <PageTemplate {...props}>
+    <PageTemplate.Navigation />
+    <PageTemplate.Title />
+    <PageTemplate.Main />
+  </PageTemplate>
 );
