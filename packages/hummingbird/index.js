00a1bba62c5914562589a834bb5f15f3f8a2fef8
Mark Jackson <mark.j.jackson@me.com>
2019-08-05 11:29:59 +0200

    feat: export CampaignConfirmedTemplate (#240)

--- packages/hummingbird/index.js
+++ packages/hummingbird/index.js
@@ -5,6 +5,9 @@ export { default as LayoutDefault } from './src/components/LayoutDefault';
 export { default as ArticleTemplate } from './src/templates/components/Article';
 export { default as PageTemplate } from './src/templates/components/Page';
 export { default as CampaignTemplate } from './src/templates/components/Campaign';
+export {
+  default as CamapaignConfirmedTemplate,
+} from './src/templates/components/CampaignConfirmed';
 
 // services
 export { default as routing } from './services/routing';
