fb34a68f5f614df7de7d66145880fb31e25ce74d
Mark Jackson <mark.j.jackson@me.com>
2019-09-13 15:38:53 +0200

    fix: fix priority and order of platforms tags (facebook > search/all)

--- packages/hummingbird/lib/wrapPageElement.js
+++ packages/hummingbird/lib/wrapPageElement.js
@@ -83,15 +83,31 @@ const PageWrapper = ({
           <meta name="description" content={platforms.all.description} />
         ) : null}
         {platforms.facebook && platforms.facebook.title ? (
-          <meta
-            property="og:title"
-            content={generateTitle(title, siteTitle, 'facebook', platforms)}
-          />
+          <React.Fragment>
+            <meta
+              property="og:title"
+              content={generateTitle(title, siteTitle, 'facebook', platforms)}
+            />
+            <meta
+              property="fb:title"
+              content={generateTitle(title, siteTitle, 'facebook', platforms)}
+            />
+          </React.Fragment>
         ) : null}
-        {platforms.all && platforms.all.description ? (
+        {platforms.facebook && platforms.facebook.description ? (
+          <React.Fragment>
+            <meta property="og:description" content={platforms.facebook.description} />
+            <meta property="fb:description" content={platforms.facebook.description} />
+          </React.Fragment>
+        ) : platforms.all && platforms.all.description ? (
           <meta property="og:description" content={platforms.all.description} />
         ) : null}
-        {platforms.all && platforms.all.imageUrl ? (
+        {platforms.facebook && platforms.facebook.imageUrl ? (
+          <React.Fragment>
+            <meta property="og:image" content={platforms.facebook.imageUrl} />
+            <meta property="fb:image" content={platforms.facebook.imageUrl} />
+          </React.Fragment>
+        ) : platforms.all && platforms.all.imageUrl ? (
           <meta property="og:image" content={platforms.all.imageUrl} />
         ) : null}
         {platforms.twitter && platforms.twitter.title ? (
@@ -107,18 +123,6 @@ const PageWrapper = ({
           <meta property="twitter:image" content={platforms.twitter.imageUrl} />
         ) : null}
         <meta name="twitter:card" content="summary_large_image" />
-        {platforms.facebook && platforms.facebook.title ? (
-          <meta
-            property="fb:title"
-            content={generateTitle(title, siteTitle, 'facebook', platforms)}
-          />
-        ) : null}
-        {platforms.facebook && platforms.facebook.description ? (
-          <meta property="fb:description" content={platforms.facebook.description} />
-        ) : null}
-        {platforms.facebook && platforms.facebook.imageUrl ? (
-          <meta property="fb:image" content={platforms.facebook.imageUrl} />
-        ) : null}
       </Helmet>
       <IntlProvider locale={localeId} messages={messages[language]}>
         {children}
