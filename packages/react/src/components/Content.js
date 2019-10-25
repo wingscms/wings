a80641a8cb17e0631c1733dbb7f3b5281d55c04a
Mark Jackson <mark.j.jackson@me.com>
2019-10-25 10:37:00 +0200

    fix: put back content styling (#267)
    * fix: put back styling
    
    * chore: remove petition-description

--- packages/react/src/components/Content.js
+++ packages/react/src/components/Content.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import includes from 'lodash.includes';
 import Renderer03 from '@dailybeast/mobiledoc-react-renderer/dist/renderers/0-3';
@@ -33,7 +34,7 @@ const convertCard = ({ View, ...card }) => ({
   render: ({ payload, ...props }) => <View {...payload} {...props} />,
 });
 
-export default class Content extends Component {
+class Content extends Component {
   static propTypes = {
     content: PropTypes.string,
     cards: PropTypes.array,
@@ -97,3 +98,111 @@ export default class Content extends Component {
     return <div {...props}>{!content ? null : this.renderer.render(JSON.parse(content))}</div>;
   }
 }
+
+export default styled(Content)`
+  font-size: 16px;
+  @media screen and (min-width: 600px) {
+    font-size: 18px;
+  }
+  @media screen and (min-width: 800px) {
+    font-size: 23px;
+  }
+  p a,
+  ol a,
+  ul a {
+    color: ${({ theme }) => theme.textColor};
+    text-decoration: none;
+    background-image: linear-gradient(
+      120deg,
+      ${({ theme }) => theme.primaryColor} 0%,
+      ${({ theme }) => theme.primaryColor} 100%
+    );
+    background-repeat: no-repeat;
+    background-size: 100% 2px;
+    background-position: 0% 100%;
+    transition: background-size 0.1s linear;
+    &:hover,
+    &:focus {
+      background-size: 100% 4px;
+      background-image: linear-gradient(
+        120deg,
+        ${({ theme }) => theme.primaryColor} 0%,
+        ${({ theme }) => theme.primaryColor} 100%
+      );
+    }
+  }
+  color: ${({ theme }) => theme.textColor};
+  padding-bottom: ${({ mini }) => (mini ? '0' : '40px')};
+  font-weight: ${({ theme }) => theme.bodyFontWeight};
+  > * {
+    margin-bottom: 0;
+    margin-top: 0;
+    & + * {
+      margin-top: 1rem;
+      @media screen and (min-width: 800px) {
+        margin-top: 1.5rem;
+      }
+    }
+    & + h2,
+    & + h3 {
+      margin-top: 1.5rem;
+      @media screen and (min-width: 800px) {
+        margin-top: 3rem;
+      }
+    }
+  }
+  & > div > p:first-child {
+    margin-top: ${({ mini, theme }) => (mini ? '0' : theme.largeSpacing)};
+    @media screen and (max-width: 800px) {
+      margin-top: ${({ mini, theme }) => (mini ? '0' : theme.mediumSpacing)};
+    }
+  }
+  &.drop-cap > div > p:first-child {
+    &::first-letter {
+      font-weight: ${({ theme }) => theme.firstLetterFontWeight}!important;
+      color: ${({ theme }) => theme.primaryColor};
+      float: left;
+      line-height: ${({ theme }) => theme.firstLetterLineHeight};
+      margin: 0.075em 0.1em -0.1em 0;
+      font-size: ${({ theme }) => theme.dropcapFontSize};
+      font-family: ${({ theme }) => theme.headingFont};
+    }
+  }
+  > ul,
+  > ol {
+    li {
+      margin: 0;
+    }
+  }
+  > blockquote {
+    border-left: 2px solid ${({ theme }) => theme.primaryColor};
+    margin: 0;
+    padding: 0 0 0 1rem;
+    color: #000;
+  }
+  > h2,
+  > h3,
+  > h4,
+  > h5,
+  > h6 {
+    color: ${({ theme }) => theme.textColor};
+    margin-bottom: 5px;
+    line-height: 1.2;
+    text-transform: none;
+    @media screen and (min-width: 800px) {
+      margin-bottom: 0;
+    }
+  }
+  &.drop-cap > div > div.headerContainer + p {
+    position: relative;
+    &::first-letter {
+      font-weight: ${({ theme }) => theme.firstLetterFontWeight}!important;
+      color: ${({ theme }) => theme.primaryColor};
+      float: left;
+      line-height: ${({ theme }) => theme.firstLetterLineHeight};
+      margin: 0.075em 0.1em -0.2em 0;
+      font-size: ${({ theme }) => theme.dropcapFontSize};
+      font-family: ${({ theme }) => theme.headingFont};
+    }
+  }
+`;
