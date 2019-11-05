1fabbd88dee388c3c313a00de921dea156281ac2
Mark Jackson <mark.j.jackson@me.com>
2019-11-05 10:40:28 +0100

    feat: add simplified header for articles with no header image (#287)

--- packages/hummingbird/src/templates/components/Article/Header/Header.js
+++ packages/hummingbird/src/templates/components/Article/Header/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import LandingSection from './LandingSection';
+import SimpleHeader from './SimpleHeader';
 
-export default ({ article, skipToContentTooltip }) => (
-  <LandingSection article={article} titleAttribute={skipToContentTooltip} />
-);
+export default ({ article, skipToContentTooltip }) => {
+  const { image } = article;
+  if (image) {
+    return <LandingSection article={article} titleAttribute={skipToContentTooltip} />;
+  }
+  return <SimpleHeader article={article} />;
+};
