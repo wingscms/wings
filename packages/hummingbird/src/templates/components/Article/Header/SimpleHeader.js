5746c0ece4a74091b9b84eb62dc99c795eb6377d
Mark Jackson <mark.j.jackson@me.com>
2019-12-16 08:37:19 +0100

    fix: review uppercaseTitles propagation
    fixes #313

--- packages/hummingbird/src/templates/components/Article/Header/SimpleHeader.js
+++ packages/hummingbird/src/templates/components/Article/Header/SimpleHeader.js
@@ -15,6 +15,8 @@ const TitleContainer = styled.div`
 const Title = styled.h1`
   letter-spacing: 1.5px;
   color: ${({ theme }) => theme.textColor};
+  text-transform: ${({ theme }) =>
+    (theme.uppercaseTitles ? 'uppercase' : 'none')};
   display: inline-block;
   font-size: 32px;
   line-height: 1.2;
@@ -38,4 +40,4 @@ export default ({ article }) => {
       <Title>{title}</Title>
     </TitleContainer>
   );
-}
+};
