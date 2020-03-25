ac8e8f3f3eb86152ed547e1dc9c5fcaed4796d15
Maarten Busstra <busstra.m@gmail.com>
2020-03-26 00:15:40 +0100

    docs: add more button stories

--- packages/crane/src/components/Button.stories.js
+++ packages/crane/src/components/Button.stories.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { text, select, boolean } from '@storybook/addon-knobs/react';
 import { Button, useTheme } from '..';
 
-const props = theme => ({
+const props = ({ theme, ...p }) => ({
   type: select('type', { normal: 'normal', outline: 'outline' }, 'normal'),
   intent: select('intent', theme.Intent, 'primary'),
-  size: select('size', Button.Size, 'medium'),
+  size: select('size', Button.Size, p.size || 'medium'),
   disabled: boolean('disabled', false),
-  loading: boolean('loading', false),
+  loading: boolean('loading', p.loading || false),
   children: text('children', 'Click me!'),
 });
 
 export default () => {
   const theme = useTheme();
-  return <Button {...props(theme)} />;
+  return <Button {...props({ theme })} />;
 };
 
 export const Loading = () => {
   const theme = useTheme();
-  return <Button {...props(theme)} loading={boolean('loading', true)} />;
+  return <Button {...props({ theme, loading: true })} />;
+};
+
+export const Small = () => {
+  const theme = useTheme();
+  return <Button {...props({ theme, size: Button.Size.SMALL })} />;
 };
