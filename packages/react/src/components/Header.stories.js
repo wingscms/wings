aad1e5d57e74903cbbf4883c04909b49e974176f
Mark Jackson <mark.j.jackson@me.com>
2020-04-01 11:48:11 +0200

    add knobs to header story

--- packages/react/src/components/Header.stories.js
+++ packages/react/src/components/Header.stories.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { text } from '@storybook/addon-knobs/react';
 import Header from './Header';
 
 export default () => (
   <Header
-    title="Test title"
+    title={text('title', 'Test title')}
     image={{
-      url: 'https://files.wings.dev/1530796123797/space-travel-1784461640.png',
-      caption: 'An image',
+      url: text(
+        'url',
+        'https://files.wings.dev/1530796123797/space-travel-1784461640.png',
+        'image',
+      ),
+      caption: text('caption', 'A test caption', 'image'),
     }}
   />
 );
