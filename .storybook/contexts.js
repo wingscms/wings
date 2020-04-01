18b13e68d54d273020c06caa5d59619c0c9fd02c
Mark Jackson <mark.j.jackson@me.com>
2020-04-01 11:12:52 +0200

    add theme context to storybook

--- .storybook/contexts.js
+++ .storybook/contexts.js
@@ -1,4 +1,6 @@
+import { ThemeProvider } from 'styled-components';
 import WingsProvider from '@wingscms/react/src/ctx/Wings';
+import { Theme } from '@wingscms/components';
 import client from './wings/wings';
 
 export default [
@@ -7,6 +9,20 @@ export default [
     title: 'Default',
     components: [WingsProvider],
     params: [{ name: 'Mock Wings API', props: { client }, default: true }],
+    options: {
+      deep: true,
+      disable: false,
+      cancelable: false,
+    },
+  },
+  {
+    icon: 'paintbrush',
+    title: 'Theme',
+    components: [ThemeProvider],
+    params: [
+      { name: 'Default Theme', props: { theme: () => new Theme() }, default: true },
+      { name: 'Test Theme', props: { theme: () => new Theme({ primaryColor: '#ff0000' }) } },
+    ],
     options: {
       deep: true,
       disable: false,
