9ca7b708980b831b8ccd2d09d457f574de36fdee
Maarten Busstra <busstra.m@gmail.com>
2020-04-02 10:52:36 +0200

    chore: fix sb build

--- .storybook/wings/bootstrap.js
+++ .storybook/wings/bootstrap.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 const schemaPath = path.join(__dirname, 'introspectionResult.json');
 
-module.exports = async () => {
+const bootstrap = async () => {
   if (!fs.existsSync(schemaPath)) {
     const client = new Wings({ domain: 'localhost' });
     const data = await client.introspect();
     fs.writeFileSync(schemaPath, JSON.stringify(data, null, 2));
   }
 };
+
+module.exports = bootstrap();
--- storybook.js
+++ storybook.js
@@ -1,7 +1,7 @@
 const storybook = require('@storybook/react/standalone');
 const bootstrap = require('./.storybook/wings/bootstrap');
 
-bootstrap().then(() =>
+bootstrap.then(() =>
   storybook({
     mode: 'dev',
     port: 8001,
