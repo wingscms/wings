33f9e4db08d1415af920e46377f9a23d7dbf0d2f
Maarten Busstra <busstra.m@gmail.com>
2020-04-02 10:25:21 +0200

    chore: add  placeholder image util

--- packages/components/src/components/Header.stories.js
+++ packages/components/src/components/Header.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { text, select } from '@storybook/addon-knobs/react';
+import { image } from '../../../../utils';
 import Header from './Header';
 
 const StoryWrap = styled.div`
@@ -11,7 +12,7 @@ const StoryWrap = styled.div`
 const props = ({ type = Header.Type.SIMPLE } = {}) => ({
   type: select('type', Header.Type, type),
   title: text('title', 'Test title'),
-  imageUrl: text('url', 'https://files.wings.dev/1530796123797/space-travel-1784461640.png'),
+  imageUrl: text('url', image(1200, 800)),
   caption: text('caption', 'A test caption'),
 });
 
--- packages/react/src/components/Page.stories.js
+++ packages/react/src/components/Page.stories.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Page from './Page';
 import richMd from '../../fixtures/mobiledocRich.json';
+import { image } from '../../../../utils';
 
 const node = {
   title: 'This is a test page',
   image: {
-    url: 'https://files.wings.dev/1530796123797/space-travel-1784461640.png',
+    url: image(800, 600),
     caption: 'A test image',
   },
   content: JSON.stringify(richMd),
