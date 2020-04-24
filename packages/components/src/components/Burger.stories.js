2ad454758e557ffd05bff517662fa505380dacd2
Mark Jackson <mark.j.jackson@me.com>
2020-04-24 16:10:26 +0200

    chore: add burger story

--- packages/components/src/components/Burger.stories.js
+++ packages/components/src/components/Burger.stories.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { number, color, boolean } from '@storybook/addon-knobs/react';
+import { number, color as _color, boolean } from '@storybook/addon-knobs/react';
 import { Burger } from '@wingscms/components';
 
-export default () => (
-  <Burger
-    active={boolean('active', false)}
-    height={number('height', 30)}
-    width={number('width', 40)}
-    barHeight={number('barHeight', 5)}
-    barBorderRadius={number('barBorderRadius', 0)}
-    color={color('color')}
-    hoverColor={color('hoverColor')}
-  />
+const props = ({ active = false, color = null, barBorderRadius = 0 } = {}) => ({
+  active: boolean('active', active),
+  height: number('height', 30),
+  width: number('width', 40),
+  barHeight: number('barHeight', 5),
+  barBorderRadius: number('barBorderRadius', barBorderRadius),
+  color: _color('color', color),
+  hoverColor: _color('hoverColor'),
+});
+
+export default () => <Burger {...props()} />;
+
+export const ActiveRed = () => (
+  <Burger {...props({ active: true, color: '#ff0000', barBorderRadius: 4 })} />
 );
