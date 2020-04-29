58efe068ef104ff56380c9c5424c96163a957697
Mark Jackson <mark.j.jackson@me.com>
2020-04-29 21:09:09 +0200

    use Heading in SimpleHeader

--- packages/components/src/components/Header/SimpleHeader.js
+++ packages/components/src/components/Header/SimpleHeader.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { t } from '../../theme';
 import { mediaUrl } from '@wingscms/sdk';
+import Heading from '../Heading';
 import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0;
+  margin-top: ${t(_ => _.mediumSpacing)};
+  @media screen and (min-width: 800px) {
+    margin-top: ${t(_ => _.largeSpacing)};
+  }
+`;
 
 const ImageContainer = styled.figure`
   margin: ${t(_ => _.mediumSpacing)} 0 0 0;
@@ -20,49 +29,21 @@ const ImageContainer = styled.figure`
   }
 `;
 
-const TitleContainer = styled.div`
-  padding: 0;
-  position: relative;
-  text-align: center;
-  margin-top: ${t(_ => _.mediumSpacing)};
-  @media screen and (min-width: 800px) {
-    margin-top: ${t(_ => _.largeSpacing)};
-  }
-`;
-
-const Title = styled.h1`
-  font-family: ${t(_ => _.headerFontFamily)};
-  letter-spacing: 1.5px;
-  color: ${t(_ => _.textColor)};
-  text-transform: ${t(_ => _.uppercaseTitles)};
-  display: inline-block;
-  font-size: 2rem;
-  line-height: 1.2;
-  text-align: center;
-  margin: 0 auto 0 auto;
-  max-width: 90%;
-  position: relative;
-  z-index: 2;
-  @media screen and (min-width: 600px) {
-    font-size: 2.5rem;
-  }
-  @media screen and (min-width: 800px) {
-    font-size: 3rem;
-  }
-  @media screen and (min-width: 960px) {
-    width: 920px;
-  }
-`;
-
 export default ({ title, imageUrl, imageCaption } = {}) => {
   return (
-    <TitleContainer>
-      <Title>{title}</Title>
+    <Container>
+      <Heading
+        rank={1}
+        textAlign={Heading.TextAlign.CENTER}
+        style={{ margin: '0 auto', width: '90%' }}
+      >
+        {title}
+      </Heading>
       {imageUrl ? (
         <ImageContainer>
           <img src={mediaUrl(imageUrl, { width: 1160 })} alt={imageCaption} />
         </ImageContainer>
       ) : null}
-    </TitleContainer>
+    </Container>
   );
 };
