d06efd691651bc8cc73e005f559c531b78defaf0
Mark Jackson <mark.j.jackson@me.com>
2020-05-06 12:08:53 +0200

    story

--- packages/react/src/components/Footer.stories.js
+++ packages/react/src/components/Footer.stories.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { color, object } from '@storybook/addon-knobs';
 import { Footer } from '@wingscms/react';
 
 import columns from '../../fixtures/footerContent.json';
 
-export default () => <Footer columns={columns} />;
+export default () => (
+  <Footer
+    backgroundColor={color('backgroundColor', '')}
+    color={color('color', '')}
+    columns={object('columns', columns)}
+  />
+);
