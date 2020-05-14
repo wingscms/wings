0be4e74f759b332affc5a946ef0c00887516f718
Maarten Busstra <busstra.m@gmail.com>
2020-05-14 17:07:48 +0200

    Update sections.js

--- packages/react/src/components/MobiledocRenderer/sections.js
+++ packages/react/src/components/MobiledocRenderer/sections.js
@@ -1,45 +1,7 @@
 import React from 'react';
 import { Heading } from '@wingscms/components';
 
-export default [
-  {
-    name: 'h2',
-    component: props => (
-      <Heading rank={2} {...props}>
-        {props.children}
-      </Heading>
-    ),
-  },
-  {
-    name: 'h3',
-    component: props => (
-      <Heading rank={3} {...props}>
-        {props.children}
-      </Heading>
-    ),
-  },
-  {
-    name: 'h4',
-    component: props => (
-      <Heading rank={4} {...props}>
-        {props.children}
-      </Heading>
-    ),
-  },
-  {
-    name: 'h5',
-    component: props => (
-      <Heading rank={5} {...props}>
-        {props.children}
-      </Heading>
-    ),
-  },
-  {
-    name: 'h6',
-    component: props => (
-      <Heading rank={6} {...props}>
-        {props.children}
-      </Heading>
-    ),
-  },
-];
+export default [1, 2, 3, 4, 5, 6].map(rank => ({
+  name: `h${rank}`,
+  component: props => <Heading rank={rank} {...props} />,
+}));
