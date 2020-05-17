7342c754d6c6b90430cbcfbcf8fb433f24c8de95
Mark Jackson <mark.j.jackson@me.com>
2020-05-17 19:55:57 +0200

    remaining card stories

---
+++ packages/react/src/components/cards/QACard.stories.js
@@ -0,0 +1,28 @@
+import { QACard } from '@wingscms/react';
+
+export default () =>
+  QACard.render({
+    type: 2,
+    title: 'Question and Answers',
+    intro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    content: [
+      {
+        question:
+          '{"version":"0.3.1","atoms":[],"cards":[],"markups":[],"sections":[[1,"h2",[[0,[],0,"Question"]]]]}',
+        answer:
+          '{"version":"0.3.1","atoms":[],"cards":[],"markups":[],"sections":[[1,"p",[[0,[],0,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]]]]}',
+      },
+      {
+        question:
+          '{"version":"0.3.1","atoms":[],"cards":[],"markups":[],"sections":[[1,"h2",[[0,[],0,"Question"]]]]}',
+        answer:
+          '{"version":"0.3.1","atoms":[],"cards":[],"markups":[],"sections":[[1,"p",[[0,[],0,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]]]]}',
+      },
+      {
+        question:
+          '{"version":"0.3.1","atoms":[],"cards":[],"markups":[],"sections":[[1,"h2",[[0,[],0,"Question"]]]]}',
+        answer:
+          '{"version":"0.3.1","atoms":[],"cards":[],"markups":[],"sections":[[1,"p",[[0,[],0,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]]]]}',
+      },
+    ],
+  });
