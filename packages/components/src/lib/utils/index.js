a4830ea0be0ef80320c8398682a3159317827f97
Mark Jackson <mark.j.jackson@me.com>
2020-05-22 14:33:48 +0200

    delete slugify from components

--- packages/components/src/lib/utils/index.js
+++ packages/components/src/lib/utils/index.js
@@ -1,4 +1,3 @@
 export { default as getViewportDimensions } from './getViewportDimensions';
 export { default as mediaQuery } from './mediaQuery';
 export { default as modularScale } from './modularScale';
-export { default as slugify } from './slugify';
--- packages/components/src/lib/utils/slugify.js
+++
@@ -1,7 +0,0 @@
-import _slugify from 'slugify';
-
-export default s =>
-  _slugify(s, {
-    remove: /[$*_+~.()'"!\-:@]/g,
-    lower: true,
-  });
