ce3f795412bbcc9f61353ee2e3b638e79c0c20e5
Mark Jackson <mark.j.jackson@me.com>
2020-05-25 15:46:54 +0200

    review Heading

--- packages/components/src/components/Heading.stories.js
+++ packages/components/src/components/Heading.stories.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { boolean, number } from '@storybook/addon-knobs/react';
+import { boolean, text, number } from '@storybook/addon-knobs/react';
 import { Heading } from '@wingscms/components';
 import { paddingWrap } from '../../../../utils';
+import defaultTheme from '../theme/defaultTheme';
 
-const props = ({ rank = 1 } = {}) => {
-  const bfs = number('baseFontSize', 0);
-  const bfst = number('baseTabletFontSize', 0);
-  const bfsm = number('baseMobileFontSize', 0);
-  return {
-    rank,
-    scaleRatio: number('scaleRatio', 1.25),
-    baseFontSize: bfs ? `${bfs}px` : null,
-    baseTabletFontSize: bfst ? `${bfst}px` : null,
-    baseMobileFontSize: bfsm ? `${bfsm}px` : null,
-    uppercase: boolean('uppercase', false),
-  };
-};
+const props = ({ rank = 1 } = {}) => ({
+  rank,
+  scaleRatio: number('scaleRatio', 1.25),
+  baseFontSize: text('baseFontSize', defaultTheme.baseFontSize),
+  baseTabletFontSize: text('baseTabletFontSize', defaultTheme.baseTabletFontSize),
+  baseMobileFontSize: text('baseMobileFontSize', defaultTheme.baseMobileFontSize),
+  uppercase: boolean('uppercase', false),
+});
 
 export default () => (
   <>
-    <Heading {...props({ rank: 1 })}>This is a Heading</Heading>
+    <Heading {...props({ rank: number('rank', 1) })}>This is a Heading</Heading>
     <Heading {...props({ rank: 2 })}>This is a Heading</Heading>
     <Heading {...props({ rank: 3 })}>This is a Heading</Heading>
     <Heading {...props({ rank: 4 })}>This is a Heading</Heading>
     <Heading {...props({ rank: 5 })}>This is a Heading</Heading>
+    <Heading {...props({ rank: 6 })}>This is a Heading</Heading>
   </>
 );
 
