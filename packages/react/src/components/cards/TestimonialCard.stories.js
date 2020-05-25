3ad8282e9faf0549a837d9f13d3554d4a910727a
Maarten Busstra <busstra.m@gmail.com>
2020-05-25 17:20:57 +0200

    Update TestimonialCard.stories.js

--- packages/react/src/components/cards/TestimonialCard.stories.js
+++ packages/react/src/components/cards/TestimonialCard.stories.js
@@ -1,15 +1,13 @@
 import faker from 'faker';
 import { TestimonialCard } from '@wingscms/react';
-import { contentWrap, mobiledocWithText } from '../../../../../utils';
+import { image, contentWrap, mobiledocWithText } from '../../../../../utils';
 
 export default () =>
   TestimonialCard.render({
-    type: 1,
-    title: 'A testimonial',
-    intro:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: faker.company.catchPhrase(),
+    intro: faker.lorem.sentence(),
     content: mobiledocWithText(faker.lorem.paragraphs(50)),
-    url: 'https://files.wings.dev/1530796123797/space-travel-1784461640.png',
+    url: image(),
   });
 
 export const wrapStory = contentWrap;
