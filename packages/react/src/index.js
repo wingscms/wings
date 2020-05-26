659f75b9a604396c63a0dcf2346aaab44968478f
Mark Jackson <mark.j.jackson@me.com>
2020-05-26 14:06:09 +0200

    remove getNodeMetaTags

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -4,28 +4,6 @@ export { slugify } from './lib/utils';
 export { default as WingsProvider, useWings, withWings } from './ctx/Wings';
 export { IntlProvider, useIntl, withIntl } from './ctx/Intl';
 
-export const getNodeMetaTags = node => {
-  const { all, facebook, twitter } = node.platforms;
-
-  return {
-    title: all.title,
-    meta: [
-      { name: 'description', content: all.description },
-
-      { property: 'og:title', content: facebook.title },
-      { property: 'og:description', content: facebook.description },
-      { property: 'og:image', content: facebook.imageUrl },
-      { property: 'og:image:secure_url', content: facebook.imageUrl },
-      { property: 'og:type', content: 'article' },
-
-      { name: 'twitter:title', content: twitter.title },
-      { name: 'twitter:description', content: twitter.description },
-      { name: 'twitter:image', content: twitter.imageUrl },
-      { name: 'twitter:card', content: 'summary_large_image' },
-    ],
-  };
-};
-
 export { default as Content } from './components/Content';
 export { default as Campaign } from './components/Campaign/Campaign';
 export { default as CampaignConfirmed } from './components/CampaignConfirmed';
