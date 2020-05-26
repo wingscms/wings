25b11d98a9ed3974c5075b44380d0314b2b594be
Mark Jackson <mark.j.jackson@me.com>
2020-05-26 14:07:37 +0200

    Update index.js

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,7 +1,7 @@
-export { default as Content } from './components/Content';
+export { default as Article } from './components/Article';
 export { default as Campaign } from './components/Campaign/Campaign';
 export { default as CampaignConfirmed } from './components/CampaignConfirmed';
-export { default as Article } from './components/Article';
+export { default as Content } from './components/Content';
 export { default as Page } from './components/Page';
 
 export {
