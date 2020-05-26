98231b3655232e56b3d03cdcb66a4cff1c3d071c
Mark Jackson <mark.j.jackson@me.com>
2020-05-26 13:38:03 +0200

    progress

--- packages/components/src/components/Collapse.js
+++ packages/components/src/components/Collapse.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import fP from 'filter-invalid-dom-props';
 import styled, { css } from '../lib/styled';
 import Theme, { t } from '../theme';
@@ -6,23 +6,20 @@ import Heading from './Heading';
 
 const Container = styled.div`
   width: 100%;
+  margin-bottom: ${t(_ => _.smallSpacing)};
 `;
 
-const Title = styled.div`
+const Label = styled.div`
   ${t((_, { intent }) => {
     const color = _.intentColor(intent);
     return css`
       background-color: ${color};
       color: ${_.contrastColor({ backgroundColor: color })};
       padding: ${_.smallSpacing};
-      ${t(Heading.getStyles(0))}
-      font-weight: bold;
-      width: 100%;
       cursor: pointer;
       opacity: 1;
       transition: all 0.15s ease-in-out;
       user-select: none;
-      margin: 0;
       &:hover,
       &:active {
         opacity: 0.8;
@@ -32,18 +29,23 @@ const Title = styled.div`
 `;
 
 const Content = styled.div`
-  padding: ${t(_ => _.smallSpacing)};
-  opacity: 1;
-  transition: all 0.15s ease-in-out;
-  height: auto;
-  ${({ open }) =>
-    !open &&
-    css`
-      height: 0;
-      padding: 0 ${t(_ => _.smallSpacing)};
-      overflow: hidden;
-      /* opacity: 0; */
-    `}
+  ${t((_, { intent }) => {
+    const color = _.intentColor(intent);
+    return css`
+      padding: ${t(_ => _.smallSpacing)};
+      opacity: 1;
+      transition: all 0.15s ease-in-out;
+      height: auto;
+      border: 1px solid ${color};
+      ${({ open }) =>
+        !open &&
+        css`
+          height: 0;
+          padding: 0 ${t(_ => _.smallSpacing)};
+          overflow: hidden;
+        `}
+    `;
+  })}
 `;
 
 export default function Collapse({
@@ -51,15 +53,25 @@ export default function Collapse({
   onClick,
   open,
   intent = Theme.Intent.NONE,
-  title,
+  label,
   ...props
 }) {
+  const [_open, setOpen] = useState(false);
+  const _onClick = () => setOpen(!_open);
+  const getOpen = () => {
+    if (typeof open === 'boolean') return open;
+    return _open;
+  };
   return (
     <Container {...fP(props)}>
-      <Title onClick={onClick} intent={intent}>
-        {title}
-      </Title>
-      <Content open={open}>{children}</Content>
+      <Label onClick={onClick || _onClick} intent={intent}>
+        <Heading noSpacing rank={4}>
+          {label}
+        </Heading>
+      </Label>
+      <Content open={getOpen()} intent={intent}>
+        {children}
+      </Content>
     </Container>
   );
 }
--- packages/components/src/components/Collapse.stories.js
+++ packages/components/src/components/Collapse.stories.js
@@ -3,15 +3,31 @@ import faker from 'faker';
 import { boolean, select } from '@storybook/addon-knobs/react';
 import { Collapse } from '@wingscms/components';
 import { paddingWrap } from '../../../../utils';
+import { text } from '@storybook/addon-knobs';
 
-export default () => {
+export const Controlled = () => {
   faker.seed(1);
   return (
     <>
       <Collapse
         intent={select('intent', Collapse.Intent, Collapse.Intent.NONE)}
         open={boolean('open', false)}
-        title="Collapse Title"
+        label={text('label', faker.lorem.sentence())}
+      >
+        {faker.lorem.paragraph(6)}
+      </Collapse>
+      {faker.lorem.paragraph(1)}
+    </>
+  );
+};
+
+export const Uncontrolled = () => {
+  faker.seed(1);
+  return (
+    <>
+      <Collapse
+        intent={select('intent', Collapse.Intent, Collapse.Intent.NONE)}
+        label={text('label', faker.lorem.sentence())}
       >
         {faker.lorem.paragraph(6)}
       </Collapse>
