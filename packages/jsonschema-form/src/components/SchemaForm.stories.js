49298a202d13970b530062ae1ea266b721d2be1c
Mark Jackson <mark.j.jackson@me.com>
2020-05-26 16:25:36 +0200

    SchemaForm story

--- packages/jsonschema-form/src/components/SchemaForm.stories.js
+++ packages/jsonschema-form/src/components/SchemaForm.stories.js
@@ -1,42 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button } from '@wingscms/components';
 import SchemaForm from '@wingscms/jsonschema-form';
+import { object } from '@storybook/addon-knobs/react';
+import { contentWrap } from '../../../../utils';
+
+const Container = styled.div`
+  background-color: rgb(65, 125, 232);
+  color: #fff;
+  border-radius: 4px;
+  padding: 20px;
+`;
 
 export default () => (
-  <SchemaForm
-    schema={{
-      title: 'A registration form',
-      description: 'A simple form example.',
-      type: 'object',
-      required: ['firstName', 'lastName'],
-      properties: {
-        firstName: {
-          type: 'string',
-          title: 'First name',
-          default: 'Chuck',
+  <Container>
+    <SchemaForm
+      schema={object('schema', {
+        title: 'A registration form',
+        description: 'A simple form example.',
+        type: 'object',
+        required: ['firstName', 'lastName'],
+        properties: {
+          firstName: {
+            type: 'string',
+            title: 'First name',
+            default: 'Chuck',
+          },
+          lastName: {
+            type: 'string',
+            title: 'Last name',
+          },
+          age: {
+            type: 'integer',
+            title: 'Age',
+          },
+          bio: {
+            type: 'string',
+            title: 'Bio',
+          },
+          password: {
+            type: 'string',
+            title: 'Password',
+            minLength: 3,
+          },
+          telephone: {
+            type: 'string',
+            title: 'Telephone',
+            minLength: 10,
+          },
         },
-        lastName: {
-          type: 'string',
-          title: 'Last name',
-        },
-        age: {
-          type: 'integer',
-          title: 'Age',
-        },
-        bio: {
-          type: 'string',
-          title: 'Bio',
-        },
-        password: {
-          type: 'string',
-          title: 'Password',
-          minLength: 3,
-        },
-        telephone: {
-          type: 'string',
-          title: 'Telephone',
-          minLength: 10,
-        },
-      },
-    }}
-  />
+      })}
+    >
+      <Button intent={Button.Intent.SECONDARY}>Submit</Button>
+    </SchemaForm>
+  </Container>
 );
+
+export const wrapStory = contentWrap;
