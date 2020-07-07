3da448bb5be77fdda8c1c5cc3a0bc33b3132b058
Maarten Busstra <busstra.m@gmail.com>
2020-07-07 13:01:14 +0200

    remove faker call

--- packages/components/src/components/Collapse.stories.js
+++ packages/components/src/components/Collapse.stories.js
@@ -5,35 +5,29 @@ import { Collapse, Text } from '@wingscms/components';
 import { paddingWrap } from '../../../../utils';
 import { text } from '@storybook/addon-knobs';
 
-export const Controlled = () => {
-  faker.seed(1);
-  return (
-    <>
-      <Collapse
-        intent={select('intent', Collapse.Intent, Collapse.Intent.NONE)}
-        open={boolean('open', false)}
-        label={text('label', faker.lorem.sentence())}
-      >
-        <Text>{faker.lorem.paragraph(6)}</Text>
-      </Collapse>
-      <Text>{faker.lorem.paragraph(1)}</Text>
-    </>
-  );
-};
+export const Controlled = () => (
+  <>
+    <Collapse
+      intent={select('intent', Collapse.Intent, Collapse.Intent.NONE)}
+      open={boolean('open', false)}
+      label={text('label', faker.lorem.sentence())}
+    >
+      <Text>{faker.lorem.paragraph(6)}</Text>
+    </Collapse>
+    <Text>{faker.lorem.paragraph(1)}</Text>
+  </>
+);
 
-export const Uncontrolled = () => {
-  faker.seed(1);
-  return (
-    <>
-      <Collapse
-        intent={select('intent', Collapse.Intent, Collapse.Intent.NONE)}
-        label={text('label', faker.lorem.sentence())}
-      >
-        <Text>{faker.lorem.paragraph(6)}</Text>
-      </Collapse>
-      <Text>{faker.lorem.paragraph(1)}</Text>
-    </>
-  );
-};
+export const Uncontrolled = () => (
+  <>
+    <Collapse
+      intent={select('intent', Collapse.Intent, Collapse.Intent.NONE)}
+      label={text('label', faker.lorem.sentence())}
+    >
+      <Text>{faker.lorem.paragraph(6)}</Text>
+    </Collapse>
+    <Text>{faker.lorem.paragraph(1)}</Text>
+  </>
+);
 
 export const wrapStory = paddingWrap;
