5172fcb0b848236b01c435862da37bf5d9946a94
Maarten Busstra <busstra.m@gmail.com>
2020-07-07 12:15:40 +0200

    remove faker seed

--- packages/components/src/components/Accordion.stories.js
+++ packages/components/src/components/Accordion.stories.js
@@ -4,38 +4,32 @@ import { number } from '@storybook/addon-knobs/react';
 import { Accordion, Text } from '@wingscms/components';
 import { paddingWrap } from '../../../../utils';
 
-export const Uncontrolled = () => {
-  faker.seed(1);
-  return (
-    <Accordion>
-      <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
-        <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
-      </Accordion.Item>
-      <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
-        <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
-      </Accordion.Item>
-      <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
-        <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
-      </Accordion.Item>
-    </Accordion>
-  );
-};
+export const Uncontrolled = () => (
+  <Accordion>
+    <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
+      <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
+    </Accordion.Item>
+    <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
+      <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
+    </Accordion.Item>
+    <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
+      <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
+    </Accordion.Item>
+  </Accordion>
+);
 
-export const Controlled = () => {
-  faker.seed(1);
-  return (
-    <Accordion openIndex={number('openIndex', null)}>
-      <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
-        <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
-      </Accordion.Item>
-      <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
-        <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
-      </Accordion.Item>
-      <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
-        <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
-      </Accordion.Item>
-    </Accordion>
-  );
-};
+export const Controlled = () => (
+  <Accordion openIndex={number('openIndex', null)}>
+    <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
+      <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
+    </Accordion.Item>
+    <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
+      <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
+    </Accordion.Item>
+    <Accordion.Item intent={Accordion.Item.Intent.PRIMARY} label={faker.lorem.sentence()}>
+      <Text noSpacing>{faker.lorem.paragraph(6)}</Text>
+    </Accordion.Item>
+  </Accordion>
+);
 
 export const wrapStory = paddingWrap;
