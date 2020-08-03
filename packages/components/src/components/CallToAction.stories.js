243556219a600b33afc63592bb481c2a3d0eebde
Maarten Busstra <maartenbusstra@icloud.com>
2020-08-03 11:27:59 +0200

    Update CallToAction.stories.js

--- packages/components/src/components/CallToAction.stories.js
+++ packages/components/src/components/CallToAction.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import faker from 'faker';
-import { boolean, select, text, color } from '@storybook/addon-knobs/react';
-import { CallToAction, Button } from '@wingscms/components';
+import { boolean, select, text } from '@storybook/addon-knobs/react';
+import { CallToAction } from '@wingscms/components';
 import { paddingWrap, image } from '../../../../utils';
 
 const props = ({
