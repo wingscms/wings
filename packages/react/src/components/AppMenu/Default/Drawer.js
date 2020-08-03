621b80ef18b22e8c675c6bb54afd67ce27f39947
Mark Jackson <mark.j.jackson@me.com>
2020-08-03 15:19:45 +0200

    Update Drawer.js

--- packages/react/src/components/AppMenu/Default/Drawer.js
+++ packages/react/src/components/AppMenu/Default/Drawer.js
@@ -71,7 +71,7 @@ export default function MenuDefaultDrawer({
                 >
                   {text}
                 </PrimaryItemDrawer>,
-                { url: url },
+                { url },
               ),
             )}
       </Drawer>
