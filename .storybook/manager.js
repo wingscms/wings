296b5b4ccd51b584b30d12c5109a627911e9fb5b
Mark Jackson <mark.j.jackson@me.com>
2020-08-03 13:02:28 +0200

    storybook actions addon

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,6 +3,7 @@ import wingsTheme from './theme/theme';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-viewport/register';
 import '@storybook/addon-contexts/register';
+import '@storybook/addon-actions/register';
 
 addons.setConfig({
   theme: wingsTheme,
--- packages/components/src/components/Dialog.stories.js
+++ packages/components/src/components/Dialog.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import faker from 'faker';
 import { boolean, select } from '@storybook/addon-knobs/react';
+import { action } from '@storybook/addon-actions';
 import { Dialog, Text } from '@wingscms/components';
 import { paddingWrap } from '../../../../utils';
 
@@ -9,10 +10,10 @@ export default () => {
     <div>
       {boolean('open', true) ? (
         <Dialog
-          clickOutsideToClose={boolean('clickOutsideToClose', true)}
+          clickOutsideToClose={true}
           overlay={boolean('overlay', true)}
           open={open}
-          onClose={() => {}}
+          onClose={action('onClose')}
           size={select('size', Dialog.Size, Dialog.Size.MEDIUM)}
           verticalAlign={select('verticalAlign', Dialog.VerticalAlign, Dialog.VerticalAlign.CENTER)}
           horizontalAlign={select(
@@ -21,7 +22,7 @@ export default () => {
             Dialog.HorizontalAlign.CENTER,
           )}
         >
-          <Dialog.Header title="This Is A Dialog" onClose={() => {}} />
+          <Dialog.Header title="This Is A Dialog" onClose={action('onClose')} />
           <Text style={{ padding: '0 20px' }}>{faker.lorem.paragraphs(1)}</Text>
         </Dialog>
       ) : null}
