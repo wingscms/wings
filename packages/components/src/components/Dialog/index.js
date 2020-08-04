bce7f7bd8702b5e6fd5890eb35cf0c57b1a9744b
Mark Jackson <mark.j.jackson@me.com>
2020-08-04 10:58:25 +0200

    fix dialog position

--- packages/components/src/components/Dialog/index.js
+++ packages/components/src/components/Dialog/index.js
@@ -95,7 +95,7 @@ const Wrapper = styled.div`
   width: 100vw;
   min-height: 100vh;
   padding: ${t(_ => _.smallSpacing)};
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   overflow-y: auto;
