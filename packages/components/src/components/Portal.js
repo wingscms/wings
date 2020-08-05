4bab04d0c76c3ef713f989b91d93f08085c93f59
Mark Jackson <mark.j.jackson@me.com>
2020-08-05 11:14:44 +0200

    Update Portal.js

--- packages/components/src/components/Portal.js
+++ packages/components/src/components/Portal.js
@@ -2,16 +2,19 @@ import { useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 export default function Portal({ id = 'wings-components-portal', children, zIndex = 100 }) {
-  const portal = useRef(document.getElementById(id) || document.createElement('div'));
+  if (typeof document !== 'undefined') {
+    const portal = useRef(document.getElementById(id) || document.createElement('div'));
 
-  if (zIndex !== portal.current.style.zIndex) {
-    portal.current.setAttribute('style', `position: relative; z-index: ${zIndex};`);
+    if (zIndex !== portal.current.style.zIndex) {
+      portal.current.setAttribute('style', `position: relative; z-index: ${zIndex};`);
+    }
+
+    if (!portal.current.parentElement) {
+      portal.current.id = id;
+      document.body.append(portal.current);
+    }
+
+    return createPortal(children, portal.current);
   }
-
-  if (!portal.current.parentElement) {
-    portal.current.id = id;
-    document.body.append(portal.current);
-  }
-
-  return createPortal(children, portal.current);
+  return null;
 }
