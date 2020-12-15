73da14d9ab44cd8e6ef8c45385bc0bf015ad7036
Mark Jackson <mark.j.jackson@me.com>
2020-12-15 11:58:15 +0100

    small stylign fixes and theme testing

--- .storybook/themes/index.js
+++ .storybook/themes/index.js
@@ -1,5 +1,6 @@
 import wings from './wings';
 import wingsDark from './wingsDark';
+import keepItSimple from './keepItSimple';
 import softPink from './softPink';
 
-export default [wings, wingsDark, softPink];
+export default [wings, wingsDark, keepItSimple, softPink];
