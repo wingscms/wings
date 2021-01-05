710b319fa596c8e7973b99864575c22edcb68f2b
Mark Jackson <mark.j.jackson@me.com>
2021-01-05 10:55:38 +0100

    fix: fix youtube embed (#579)

--- packages/react/src/components/cards/EmbedCard.stories.js
+++ packages/react/src/components/cards/EmbedCard.stories.js
@@ -1,11 +1,10 @@
 import { EmbedCard } from '@wingscms/react';
+import { text } from '@storybook/addon-knobs';
 import { contentWrap } from '../../../../../utils';
 
 export default () =>
   EmbedCard.render({
-    src: 'https://www.youtube.com/watch?v=ssgfjn8zHNI',
-    html:
-      '<iframe width="480" height="270" src="https://www.youtube.com/embed/ssgfjn8zHNI?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+    src: text('src', 'https://www.youtube.com/watch?v=ssgfjn8zHNI'),
   });
 
 export const wrapStory = contentWrap;
